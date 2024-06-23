d6e746b712146d336fddcb71f4edfc484369575d
Sugarplum1827 <sugarplum@github.com>
2024-06-23 22:09:58 +0800

    added a column for valid_id in the user table and form

--- .db/CREATE TABLE reviews (.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE reviews (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    username VARCHAR(255) NOT NULL,
-    rating INT CHECK (rating >= 1 AND rating <= 5),
-    review_text TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
--- app/ajax/search.php
+++ app/ajax/search.php
@@ -2,66 +2,59 @@
 
 session_start();
 
-# check if the user is logged in
+# Check if the user is logged in
 if (isset($_SESSION['username'])) {
-    # check if the key is submitted
-    if(isset($_POST['key'])){
-       # database connection file
-	   include '../db.conn.php';
+    # Check if the key is submitted
+    if (isset($_POST['key'])) {
+        # Database connection file
+        include '../db.conn.php';
 
-	   # creating simple search algorithm :) 
-	   $key = "%{$_POST['key']}%";
-     
-	   $sql = "SELECT * FROM users
-	           WHERE username
-	           LIKE ? OR name LIKE ?";
-       $stmt = $conn->prepare($sql);
-       $stmt->execute([$key, $key]);
+        # Creating a simple search algorithm
+        $key = "%{$_POST['key']}%";
 
-       if($stmt->rowCount() > 0){ 
-         $users = $stmt->fetchAll();
+        $sql = "SELECT * FROM users
+                WHERE username LIKE ? OR name LIKE ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$key, $key]);
 
-         foreach ($users as $user) {
-         	if ($user['user_id'] == $_SESSION['user_id']) continue;
-       ?>
-       <li class="list-group-item">
-		<a href="chat.php?user=<?=$user['username']?>"
-		   class="d-flex
-		          align-items-center p-2">
-			<div class="d-flex
-			            align-items-center">
+        if ($stmt->rowCount() > 0) { 
+            $users = $stmt->fetchAll();
 
-			    <img src="uploads/<?=$user['p_p']?>"
-			         class="w-10 rounded-circle">
-
-			    <h3 class="fs-xs m-2">
-			    	<?=$user['name']?>
-			    </h3>
-
-			    <!-- Add call and review icons -->
-			    <div class="ml-2">
-			      <a href="#" title="Call">
-			        <i class="fa fa-phone fa-lg text-primary"></i>
-			      </a>
-			      <a href="reviews.php" title="Review">
-			        <i class="fa fa-star fa-lg text-warning"></i>
-			      </a>
-			    </div>
-
-			</div>
-		 </a>
-	   </li>
-       <?php } }else { ?>
-         <div class="alert alert-info 
-    				 text-center">
-		   <i class="fa fa-user-times d-block fs-big"></i>
-           The user "<?=htmlspecialchars($_POST['key'])?>"
-           is  not found.
-		</div>
-    <?php }
+            foreach ($users as $user) {
+                if ($user['user_id'] == $_SESSION['user_id']) continue;
+?>
+<li class="list-group-item">
+    <a href="chat.php?user=<?= htmlspecialchars($user['username']) ?>"
+       class="d-flex align-items-center p-2">
+        <div class="d-flex align-items-center">
+            <img src="uploads/<?= htmlspecialchars($user['p_p']) ?>"
+                 class="w-10 rounded-circle">
+            <h3 class="fs-xs m-2"><?= htmlspecialchars($user['name']) ?></h3>
+            <!-- Add call and review icons -->
+            <div class="ml-2 d-flex align-items-center">
+                <a href="#" title="Call" class="mr-2">
+                    <i class="fa fa-phone fa-lg text-primary"></i>
+                </a>
+                <a href="reviews.php" title="Review">
+                    <i class="fa fa-star fa-lg text-warning"></i>
+                </a>
+            </div>
+        </div>
+    </a>
+</li>
+<?php 
+            } 
+        } else { 
+?>
+<div class="alert alert-info text-center">
+    <i class="fa fa-user-times d-block fs-big"></i>
+    The user "<?= htmlspecialchars($_POST['key']) ?>" is not found.
+</div>
+<?php 
+        }
     }
-
-}else {
-	header("Location: ../../index.php");
-	exit;
+} else {
+    header("Location: ../../index.php");
+    exit;
 }
+?>
